3ae8ffcc85220180aa0993f48f653456300d8aa9
Bagus Mahendra Wicaksono <bagusmwicaksono@gmail.com>
2024-02-28 01:22:01 +0000

    Initial

---
+++ src/test/java/com/bagusmwicaksono/daisyduckproject/moments/MomentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bagusmwicaksono.daisyduckproject.moments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
