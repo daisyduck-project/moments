0222f78711d36d476d9cbb74e2d02c7fcc781c54
Bagus Mahendra Wicaksono <bagusmwicaksono@gmail.com>
2024-02-28 05:34:33 +0000

    Create Post

--- src/main/java/com/bagusmwicaksono/daisyduckproject/moments/controller/PostsController.java
+++ src/main/java/com/bagusmwicaksono/daisyduckproject/moments/controller/PostsController.java
@@ -2,6 +2,8 @@ package com.bagusmwicaksono.daisyduckproject.moments.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +12,7 @@ import com.bagusmwicaksono.daisyduckproject.moments.service.PostsService;
 
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("v1/posts")
@@ -26,5 +29,12 @@ public class PostsController {
     public Flux<PostsDto> getAllPosts() {
         log.info("[PostsController] getAllPosts");
         return postsService.getAllPosts();
+    }
+
+    @PostMapping("")
+    @CrossOrigin(origins = "*")
+    public Mono<PostsDto> createPost(@RequestBody PostsDto newDto){
+        log.info("[PostsController] createPost credDto="+newDto.toString());
+        return postsService.performCreatePost(newDto);
     }
 }
--- src/main/java/com/bagusmwicaksono/daisyduckproject/moments/service/PostsService.java
+++ src/main/java/com/bagusmwicaksono/daisyduckproject/moments/service/PostsService.java
@@ -4,10 +4,12 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import com.bagusmwicaksono.daisyduckproject.moments.controller.dto.PostsDto;
+import com.bagusmwicaksono.daisyduckproject.moments.model.Posts;
 import com.bagusmwicaksono.daisyduckproject.moments.repository.PostsRepository;
 
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 @Slf4j
@@ -24,6 +26,16 @@ public class PostsService {
             PostsDto dto = new PostsDto();
             BeanUtils.copyProperties(cred, dto);
             return dto;
+        });
+    }
+    public Mono<PostsDto> performCreatePost(PostsDto newDto){
+        log.info("[CredentialsService] performCreateCredential");
+        Posts newPost = new Posts();
+        BeanUtils.copyProperties(newDto,newPost);
+        return postsRepository.save(newPost).map(savePost ->{
+            PostsDto saveDto = new PostsDto();
+            BeanUtils.copyProperties(savePost, saveDto);
+            return saveDto;
         });
     }
 }
